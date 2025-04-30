ea9732b40e3de95671f0c377ececd521fecc0781
kennyzhengkehong <leo.lv@kehong-inno.com>
2025-04-30 16:32:20 +0800

    leo modified it.

---
+++ SwiftPackageTest/Sources/SwiftPackageTest/SwiftPackageTest.swift
@@ -0,0 +1,7 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+public struct Hello {
+    public static func sayHi() -> String {
+        return "Hello from my library! SZ LEO (iREd.com)"
+    }
+}
